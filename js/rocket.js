ca93eb3c6f52abe7409c2860592f856788cf87cf
Lumillule <150189382+Lumillule@users.noreply.github.com>
2023-11-13 17:07:52 +0100

    Made my rocket!

---
+++ js/rocket.js
@@ -0,0 +1,29 @@
+var timer = null;
+var countDownNumber = 10;
+
+var changeState = function(state) {
+	document.body.className = 'body-state'+state;
+	clearInterval(timer); //if there's a timer, it gets cleared when we change states
+	countDownNumber = 10;
+	document.getElementById('countdown').innerHTML = countDownNumber;
+
+	if (state == 2){
+		timer = setInterval(function(){
+			countDownNumber = countDownNumber-1; //starts before so the timer doesn't stick on 10
+			document.getElementById('countdown').innerHTML = countDownNumber; //changes the html text for our variable value
+			if (countDownNumber <= 0){ //if we reach 0 after a loop, we change state
+				changeState(3);
+			}
+		}, 500); //runs the loop every half second (1s=1000ms)
+	} else if (state == 3){
+		var success = setTimeout(function(){ //same as the interval but only lasts a certain time
+			var randomNumber = Math.round(Math.random()*10);
+			//success
+			if (randomNumber > 5){
+				changeState(4);
+			} else {
+				changeState(5); //oh no!!!
+			}
+		}, 2000);
+	};
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+//this is how you comment in JS
+
+var doCoolStuff= function () {
+	var currentClassName = document.getElementById('cool').className;
+		if (currentClassName == 'cool') {
+			document.getElementById('cool').className = 'cool red';
+		} else {
+			document.getElementById('cool').className = 'cool';
+		}
+
+} 
+
 var car = {
 			make: 'VW',
 			type : 'Polo',
